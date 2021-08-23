af05c3cb442943671e439c8186ced35326ad1b0f
freestyle385 <freestyle385@gmail.com>
2021-08-23 22:03:35 +0900

    구조 개선(Article과 Member에 Controller 각각 도입)_0823

--- src/project/App.java
+++ src/project/App.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import project_controller.ArticleController;
+import project_controller.MemberController;
 import project_dto.Article;
 import project_dto.Member;
 import project_util.Util;
@@ -24,6 +26,9 @@ public class App {
 		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
+		
+		MemberController memberController = new MemberController(sc, members);
+		ArticleController articleController = new ArticleController(sc, articles);
 
 		while (true) {
 			System.out.printf("입력하실 명령어 )) ");
@@ -38,148 +43,22 @@ public class App {
 				break;
 
 			} else if (command.equals("member join")) {
-				int id = members.size() + 1;
-				String regDate = Util.getNowDateStr();
-				String loginId = null;
-
-				while (true) {
-					System.out.print("가입하실 아이디 : ");
-					loginId = sc.nextLine();
-
-					if (isJoinableLoginId(loginId) == false) {
-						System.out.printf("%s는(은) 이미 사용중인 아이디입니다.\n", loginId);
-						continue;
-					}
-					break;
-				}
-
-				String loginPw = null;
-				String loginPwConfirm = null;
-
-				while (true) {
-					System.out.print("로그인 비밀번호 : ");
-					loginPw = sc.nextLine();
-
-					System.out.print("로그인 비밀번호 확인 : ");
-					loginPwConfirm = sc.nextLine();
-
-					if (loginPw.equals(loginPwConfirm) == false) {
-						System.out.println("비밀번호를 다시 입력해주세요.");
-						continue;
-					}
-					break;
-				}
-				System.out.print("이름 : ");
-				String name = sc.nextLine();
-
-				Member member = new Member(id, regDate, loginId, loginPw, name);
-				members.add(member);
-
-				System.out.printf("%d번 회원이 생성되었습니다.\n", id);
+				memberController.doJoin();
 
 			} else if (command.equals("article write")) {
-				int id = articles.size() + 1;
-				String regDate = Util.getNowDateStr();
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-
-				Article article = new Article(id, regDate, title, body, 0);
-				articles.add(article);
-
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				articleController.doWrite();
 
 			} else if (command.startsWith("article list ")) {
-				// 게시물 리스팅 시 필터링할 키워드를 searchKeyword로 추출
-				// 필터링된 게시물만 forListArticles 배열에 추가
-				String searchKeyword = command.substring("article list".length()).trim();
-
-				List<Article> forListArticles = articles;
-
-				if (searchKeyword.length() > 0) {
-					// searchKeyword가 유효할 때, searchKeyword가 포함된 게시물을 forListArticles의 새로 생성된 배열에 추가
-					forListArticles = new ArrayList<>();
-
-					for (Article article : articles) {
-						if (article.title.contains(searchKeyword)) {
-							forListArticles.add(article);
-						}
-					}
-					if (forListArticles.size() == 0) {
-						System.out.println("검색결과가 존재하지 않습니다.");
-						continue;
-					}
-				}
-
-				if (articles.size() == 0) {
-					System.out.println("게시글이 없습니다.");
-					continue;
-				}
-				System.out.println("번호 | 조회| 제목");
-
-				for (int i = forListArticles.size() - 1; i >= 0; i--) {
-					Article article = forListArticles.get(i);
-
-					System.out.printf("%4d|%4d|%s\n", article.id, article.hit, article.title);
-				}
+				articleController.showList(command);
 
 			} else if (command.startsWith("article detail ")) {
-				String[] commandBits = command.split(" ");
-
-				int id = Integer.parseInt(commandBits[2]);
-
-				Article foundArticle = getArticleById(id);
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-					continue;
-				}
-
-				foundArticle.increaseHit();
-
-				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", foundArticle.regDate);
-				System.out.printf("제목 : %s\n", foundArticle.title);
-				System.out.printf("내용 : %s\n", foundArticle.body);
-				System.out.printf("조회수 : %d\n", foundArticle.hit);
+				articleController.showDetail(command);
 
 			} else if (command.startsWith("article modify ")) {
-				String[] commandBits = command.split(" ");
-
-				int id = Integer.parseInt(commandBits[2]);
-
-				Article foundArticle = getArticleById(id);
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-					continue;
-				}
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-
-				foundArticle.title = title;
-				foundArticle.body = body;
-
-				System.out.printf("%d번 글이 수정되었습니다.\n", id);
+				articleController.doModify(command);
 
 			} else if (command.startsWith("article delete ")) {
-				String[] commandBits = command.split(" ");
-
-				int id = Integer.parseInt(commandBits[2]);
-
-				int foundIndex = getArticleIndexById(id);
-
-				if (foundIndex == -1) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
-					continue;
-
-				}
-				articles.remove(foundIndex);
-				System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
+				articleController.doDelete(command);
 
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
@@ -191,53 +70,6 @@ public class App {
 
 	}
 
-	private boolean isJoinableLoginId(String loginId) {
-		// getMemberIndexByLoginId의 리턴값(배열 인덱스)을 통해 가입할 아이디의 중복여부를 나타냄
-		int index = getMemberIndexByLoginId(loginId);
-
-		if (index == -1) {
-			return true;
-		}
-		return false;
-	}
-
-	private int getMemberIndexByLoginId(String loginId) {
-		// loginId를 통해 회원의 배열 인덱스를 알아냄(0 ~ )
-		// 중복된 아이디가 없을 경우 인덱스 범위 외의 -1를 반환
-		int i = 0;
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-
-	private int getArticleIndexById(int id) {
-		// id를 통해 게시물의 배열 인덱스를 알아냄(0 ~ )
-		// 찾는 게시물이 없을 경우 인덱스 범위 외의 -1를 반환
-		int i = 0;
-
-		for (Article article : articles) {
-			if (article.id == id) {
-				return i;
-			}
-			i++;
-		}
-		return -1;
-	}
-
-	private Article getArticleById(int id) {
-		// getArticleIndexById의 리턴값(배열 인덱스)에 해당하는 데이터 찾기
-		int index = getArticleIndexById(id);
-
-		if (index != -1) {
-			return articles.get(index);
-		}
-		return null;
-	}
-
 	private static void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
 
@@ -245,4 +77,6 @@ public class App {
 		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
 		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
 	}
+
+	
 }
---
+++ src/project_controller/MemberController.java
@@ -0,0 +1,81 @@
+package project_controller;
+
+import java.util.List;
+import java.util.Scanner;
+
+import project_dto.Member;
+import project_util.Util;
+
+public class MemberController {
+	private Scanner sc;
+	private List<Member> members;
+
+	public MemberController(Scanner sc, List<Member> members) {
+		this.sc = sc;
+		this.members = members;
+	}
+
+	public void doJoin() {
+		int id = members.size() + 1;
+		String regDate = Util.getNowDateStr();
+		String loginId = null;
+
+		while (true) {
+			System.out.print("가입하실 아이디 : ");
+			loginId = sc.nextLine();
+
+			if (isJoinableLoginId(loginId) == false) {
+				System.out.printf("%s는(은) 이미 사용중인 아이디입니다.\n", loginId);
+				continue;
+			}
+			break;
+		}
+
+		String loginPw = null;
+		String loginPwConfirm = null;
+
+		while (true) {
+			System.out.print("로그인 비밀번호 : ");
+			loginPw = sc.nextLine();
+
+			System.out.print("로그인 비밀번호 확인 : ");
+			loginPwConfirm = sc.nextLine();
+
+			if (loginPw.equals(loginPwConfirm) == false) {
+				System.out.println("비밀번호를 다시 입력해주세요.");
+				continue;
+			}
+			break;
+		}
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+
+		Member member = new Member(id, regDate, loginId, loginPw, name);
+		members.add(member);
+
+		System.out.printf("%d번 회원이 생성되었습니다.\n", id);
+	}
+
+	private boolean isJoinableLoginId(String loginId) {
+		// getMemberIndexByLoginId의 리턴값(배열 인덱스)을 통해 가입할 아이디의 중복여부를 나타냄
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		// loginId를 통해 회원의 배열 인덱스를 알아냄(0 ~ )
+		// 중복된 아이디가 없을 경우 인덱스 범위 외의 -1를 반환
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+}
