3eb723d2e9915899cd7583c998111d4865004b31
freestyle385 <freestyle385@gmail.com>
2021-09-13 21:36:13 +0900

    게시물 작성 시 로그인된 회원 id를 memberId(작성자)로 저장, 게시물 작성/수정/삭제 권한 제한_0913

--- src/project_controller/ArticleController.java
+++ src/project_controller/ArticleController.java
@@ -31,12 +31,24 @@ public class ArticleController extends Controller {
 			showDetail();
 			break;
 		case "write":
+			if (loginedMember == null) {
+				System.out.println("로그인 후 이용해주세요.");
+				break;
+			}
 			doWrite();
 			break;
 		case "modify":
+			if (loginedMember == null) {
+				System.out.println("로그인 후 이용해주세요.");
+				break;
+			}
 			doModify();
 			break;
 		case "delete":
+			if (loginedMember == null) {
+				System.out.println("로그인 후 이용해주세요.");
+				break;
+			}
 			doDelete();
 			break;
 		default:
@@ -48,9 +60,9 @@ public class ArticleController extends Controller {
 	public void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
 
-		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 11));
-		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
-		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
+		articles.add(new Article(1, Util.getNowDateStr(), 1, "제목1", "내용1", 11));
+		articles.add(new Article(2, Util.getNowDateStr(), 1, "제목2", "내용2", 22));
+		articles.add(new Article(3, Util.getNowDateStr(), 2, "제목3", "내용3", 33));
 	}
 
 	public ArticleController(Scanner sc, List<Article> articles) {
@@ -61,13 +73,14 @@ public class ArticleController extends Controller {
 	private void doWrite() {
 		int id = articles.size() + 1;
 		String regDate = Util.getNowDateStr();
+
 		System.out.printf("제목 : ");
 		String title = sc.nextLine();
 
 		System.out.printf("내용 : ");
 		String body = sc.nextLine();
 
-		Article article = new Article(id, regDate, title, body, 0);
+		Article article = new Article(id, regDate, loginedMember.id, title, body, 0);
 		articles.add(article);
 
 		System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -99,12 +112,12 @@ public class ArticleController extends Controller {
 			System.out.println("게시글이 없습니다.");
 			return;
 		}
-		System.out.println("번호 | 조회| 제목");
+		System.out.println("번호 | 조회 |   제목  | 작성자");
 
 		for (int i = forListArticles.size() - 1; i >= 0; i--) {
 			Article article = forListArticles.get(i);
 
-			System.out.printf("%4d|%4d|%s\n", article.id, article.hit, article.title);
+			System.out.printf("%4d|%5d|%7s|%5d\n", article.id, article.hit, article.title, article.memberId);
 		}
 	}
 
@@ -124,6 +137,7 @@ public class ArticleController extends Controller {
 
 		System.out.printf("번호 : %d\n", foundArticle.id);
 		System.out.printf("날짜 : %s\n", foundArticle.regDate);
+		System.out.printf("작성자 : %d\n", foundArticle.memberId);
 		System.out.printf("제목 : %s\n", foundArticle.title);
 		System.out.printf("내용 : %s\n", foundArticle.body);
 		System.out.printf("조회수 : %d\n", foundArticle.hit);
@@ -140,6 +154,12 @@ public class ArticleController extends Controller {
 			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 			return;
 		}
+
+		if (loginedMember.id != foundArticle.memberId) {
+			System.out.printf("%d번 게시물을 수정할 권한이 없습니다.\n", foundArticle.id);
+			return;
+		}
+		
 		System.out.printf("제목 : ");
 		String title = sc.nextLine();
 		System.out.printf("내용 : ");
@@ -156,14 +176,19 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(commandBits[2]);
 
-		int foundIndex = getArticleIndexById(id);
+		Article foundArticle = getArticleById(id);
 
-		if (foundIndex == -1) {
+		if (foundArticle == null) {
 			System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 			return;
-
 		}
-		articles.remove(foundIndex);
+		
+		if (loginedMember.id != foundArticle.memberId) {
+			System.out.printf("%d번 게시물을 삭제할 권한이 없습니다.\n", foundArticle.id);
+			return;
+		}
+		
+		articles.remove(foundArticle);
 		System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
 	}
 
--- src/project_controller/Controller.java
+++ src/project_controller/Controller.java
@@ -1,5 +1,12 @@
 package project_controller;
 
+import project_dto.Member;
+
 public abstract class Controller {
+	
+	public static Member loginedMember;
+	
 	public abstract void doAction(String command, String actionMethodName);
+	
+	public abstract void makeTestData();
 }
